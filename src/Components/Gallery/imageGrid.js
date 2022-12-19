390528ded87dac4ca9324f3672227de2da316dc2
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-12-19 17:29:40 +0100

    image grid

--- src/Components/Gallery/imageGrid.js
+++ src/Components/Gallery/imageGrid.js
@@ -4,6 +4,10 @@ function ImageGrid({ serverImages }) {
         <div>
             {serverImages && serverImages?.map(x => {
                 return (
+                    // <div>
+                    //     <img src={x.original}></img>
+                    //     <span>{x.original}</span>
+                    // </div>
                     <img src={x.original}></img>
                 )
             })}
